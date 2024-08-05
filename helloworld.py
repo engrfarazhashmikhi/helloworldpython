2c6a8a3e709ffd96b03a7e1ef0e06517ec4f65bd
engrfarazhashmikhi <farazhashmi90000@gmail.com>
2024-08-05 20:31:37 +0500

    Functions Class from functions.py file has been imported into the main helloworld.py file;

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,11 @@
+from functions import Functions
 class HelloWorld:
     def __init__(self):
         pass
     def displ(self):
-        print("Assalam-O-Alaikum WorlD")
+        # print("Assalam-O-Alaikum WorlD")
+        fun = Functions()
+        print(fun.displ("Syed Muhammad Faraz Hashmi"))
 
 hw = HelloWorld()
 hw.displ()
