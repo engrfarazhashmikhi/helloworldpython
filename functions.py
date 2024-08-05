d341fe1f12c6cd9f63405a43909e7bd54ca26c47
engrfarazhashmikhi <farazhashmi90000@gmail.com>
2024-08-05 20:33:03 +0500

    functions.py file for Functions class;

---
+++ functions.py
@@ -0,0 +1,10 @@
+class Functions:
+    def __init__(self):
+        pass
+    def displ(self, enteryourname):
+        getName = str(enteryourname)
+        statement = "Assalam-O-Alaikum "+getName+"."
+        return statement
+    
+        
+        
