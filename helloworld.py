e8ff5e76d4bfb1971b3af228d837b8ff2b4e750b
engrfarazhashmikhi <farazhashmi90000@gmail.com>
2025-01-19 19:02:59 +0500

    Function Tested Successfull;

--- helloworld.py
+++ helloworld.py
@@ -7,8 +7,9 @@ class HelloWorld:
         fun = Functions()
         print(fun.displ("Syed Muhammad Faraz Hashmi"))
 
-hw = HelloWorld()
-hw.displ()
+# hw = HelloWorld()
+# hw.displ()
 fun = Functions()
 # fun.namazCalculator(1990)
-print(fun.displ(enteryourname="SMFH"))
+# print(fun.displ(enteryourname="SMFH"))
+print("Price per item is: ", str(fun.costCalculator(fun.getInputFromUser(enterStatement="Enter the price of the product: "), fun.getInputFromUser(enterStatement="Enter total quantity of thr items: "))))
