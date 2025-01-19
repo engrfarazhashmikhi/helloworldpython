bb0481ea315d0c623c2822da06177b3cfe63cda2
engrfarazhashmikhi <farazhashmi90000@gmail.com>
2025-01-19 19:17:01 +0500

    Typo corrected;

--- helloworld.py
+++ helloworld.py
@@ -12,4 +12,4 @@ class HelloWorld:
 fun = Functions()
 # fun.namazCalculator(1990)
 # print(fun.displ(enteryourname="SMFH"))
-print("Price per item is: ", str(fun.costCalculator(fun.getInputFromUser(enterStatement="Enter the price of the product: "), fun.getInputFromUser(enterStatement="Enter total quantity of thr items: "))))
+print("Price per item is: ", str(fun.costCalculator(fun.getInputFromUser(enterStatement="Enter the price of the product: "), fun.getInputFromUser(enterStatement="Enter total quantity of the items: "))))
